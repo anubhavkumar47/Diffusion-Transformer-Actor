d91e5bc2a604d4e1b7ae6e3086d315320f492c1b
Anubhav Kumar <151345311+anubhavkumar47@users.noreply.github.com>
2025-06-06 18:00:26 +0530

    Update enviroment.py

--- enviroment.py
+++ enviroment.py
@@ -118,7 +118,7 @@ class Environment:
             r_P -= 99  # Large penalty for going out of bounds
 
         else :
-           r_P = 0
+           r_P = 10
 
         # Update positions
         past_position_A = self.current_position_A
